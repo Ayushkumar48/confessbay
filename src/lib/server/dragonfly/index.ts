fdb966bcd5d516ba146178978bc86e38e338a7f8
Ayush Kumar <ayushsuperstar48@gmail.com>
2025-12-27 14:07:18 +0530

    fix: minor chnage

--- src/lib/server/dragonfly/index.ts
+++ src/lib/server/dragonfly/index.ts
@@ -1,21 +1,14 @@
 import Redis from 'ioredis';
-import { building } from '$app/environment';
 
-let dragonfly: Redis | null = null;
+export const dragonfly = new Redis({
+	host: '127.0.0.1',
+	port: 6380
+});
 
-if (!building) {
-	dragonfly = new Redis({
-		host: '127.0.0.1',
-		port: 6380
-	});
+dragonfly.on('connect', () => {
+	console.log('🟢 Dragonfly connected');
+});
 
-	dragonfly.on('connect', () => {
-		console.log('🟢 Dragonfly connected');
-	});
-
-	dragonfly.on('error', (err) => {
-		console.error('🔴 Dragonfly error', err);
-	});
-}
-
-export { dragonfly };
+dragonfly.on('error', (err) => {
+	console.error('🔴 Dragonfly error', err);
+});
